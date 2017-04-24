b42cc6c37aa444b626ed6055036e47e4e28d4620
brian_huang <brian.huang@eitc.com.tw>
2017-04-24 11:15:37 +0800

    modified:   app_con.js

--- app_con.js
+++ app_con.js
@@ -25,22 +25,22 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.text(session, "Hello... What's your issue?");
     },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                     "you've been programming for " + session.userData.coding + 
-                     "years and use " + session.userData.language + ".");
-    },
+//    function (session, results) {
+//        session.userData.name = results.response;
+//        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+//    },
+//    function (session, results) {
+//        session.userData.coding = results.response;
+//        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+//    },
+//    function (session, results) {
+//        session.userData.language = results.response.entity;
+//        session.send("Got it... " + session.userData.name + 
+//                     "you've been programming for " + session.userData.coding + 
+//                     "years and use " + session.userData.language + ".");
+//    },
     function (session, results) {
         session.userData.issues = results.response;
         var cmd = 'curl -v -H "Content-Type: application/xml" -X POST --data-binary "@issues.xml" -u "eitc:secret"  https://b72c4b06.ngrok.io/issues.xml?key=678fb5bd075ebee0a4636b74857cb6b0ece71cf3';
