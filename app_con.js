afb7ca04ff8c4d379ceb907655c8b738742735ff
Brian Huang <brian.huang@eitc.com.tw>
2017-04-25 06:04:18 +0000

    add condition to compare responses

--- app_con.js
+++ app_con.js
@@ -25,7 +25,7 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your issue?");
+        builder.Prompts.choice(session, "Hello... Any problem?", ["Submit issue", "Just say hello"]);
     },
 //    function (session, results) {
 //        session.userData.name = results.response;
@@ -42,15 +42,20 @@ var bot = new builder.UniversalBot(connector, [
 //                     "years and use " + session.userData.language + ".");
 //    },
     function (session, results) {
-        session.userData.issues = results.response;
-        var cmd = 'curl -v -H "Content-Type: application/xml" -X POST --data-binary "@/app/issues.xml" -u "eitc:secret"  https://b72c4b06.ngrok.io/issues.xml?key=678fb5bd075ebee0a4636b74857cb6b0ece71cf3';
-        exec(cmd, function(error, stdout, stderr) {
-            session.send("cmd = " + cmd);
-            session.send("Got it... " + session.userData.issues );
-            // command output is in stdout
-            console.log("error : " + error);
-            console.log("stdout : " + stdout);
-            console.log("stderr : " + stderr);
-        });
+        session.userData.issues = results.response.entity;
+        if session.userData.issues == "Submit issue" {
+            var cmd = 'curl -v -H "Content-Type: application/xml" -X POST --data-binary "@/app/issues.xml" -u "eitc:secret"  https://b72c4b06.ngrok.io/issues.xml?key=678fb5bd075ebee0a4636b74857cb6b0ece71cf3';
+            exec(cmd, function(error, stdout, stderr) {
+                session.send("cmd = " + cmd);
+                session.send("Got it... " + session.userData.issues );
+                // command output is in stdout
+                console.log("error : " + error);
+                console.log("stdout : " + stdout);
+                console.log("stderr : " + stderr);
+            });
+        }
+        else {
+        session.send("Hello...");
+        }
     }
 ]);
