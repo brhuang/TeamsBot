21b376610cc7d242e9bd472e03d7ec706a4d29d7
Brian Huang <brian.huang@eitc.com.tw>
2017-04-25 06:45:59 +0000

    process issue subject name

--- app_con.js
+++ app_con.js
@@ -53,6 +53,15 @@ var bot = new builder.UniversalBot(connector, [
             function (session) {
                 builder.Prompts.priority(session, "Please provide the priority");
             }
+            var sedcmd = 'sed -i -c "s/this is the 10th bug for Alfred./' + builder.Prompts.subject + '/g" /app/issues.xml';
+            exec(sedcmd, function(error, stdout, stderr) {
+                session.send("cmd = " + cmd);
+                session.send("Got it... " + session.userData.issues );
+                // command output is in stdout
+                console.log("error : " + error);
+                console.log("stdout : " + stdout);
+                console.log("stderr : " + stderr);
+            });
             var cmd = 'curl -v -H "Content-Type: application/xml" -X POST --data-binary "@/app/issues.xml" -u "eitc:secret"  https://b72c4b06.ngrok.io/issues.xml?key=678fb5bd075ebee0a4636b74857cb6b0ece71cf3';
             exec(cmd, function(error, stdout, stderr) {
                 session.send("cmd = " + cmd);
